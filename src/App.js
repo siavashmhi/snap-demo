fdbaf7e809e6c8410f6a8f2bad53ba8262cf0c0c
siavash mohseni <siavashmohsenimhi79@gmail.com>
2022-09-13 01:06:49 +0430

    create navbar components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 import './styles/fonts.css';
 import './styles/App.css';
 
 function App() {
   return (
     <div>
-      <Footer />
+      <Navbar />
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import Button from '../shared/button/Button.jsx'
+import logo from '../../assets/images/logo.svg'
+import './navbar.scss'
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <header className='main-header'>
+            <nav className='nav nav-container'>
+                <div className='logo'>
+                    <img src={logo} alt='logo' />
+                </div>
+                <div className='menu'>menu</div>
+                <ul className='nav__list'>
+                    <li className='nav__item'><a href="#"> پشتیبانی </a></li>
+                    <li className='nav__item'><a href="#"> خدمات کسب و کارها </a></li>
+                    <li className='nav__item'><a href="#"> فرصت های شغلی  </a></li>
+                    <li className='nav__item'><a href="#"> وبلاگ </a></li>
+                    <li className='nav__item btn__container'> 
+                        <Button className='btn btn--primary' text='دانلود تپسی' />
+                    </li>
+                </ul>
+            </nav>
+        </header>
     );
 };
 
