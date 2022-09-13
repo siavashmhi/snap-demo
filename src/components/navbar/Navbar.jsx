30d84ab9f59a04b616e3c7ba4ecb917dbf978180
siavash mohseni <siavashmohsenimhi79@gmail.com>
2022-09-13 18:54:31 +0430

    finish navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import Button from '../shared/button/Button.jsx'
 import logo from '../../assets/images/logo.svg'
 import './navbar.scss'
 
 const Navbar = () => {
+
+    const [navMenu, setNavMenu] =  useState(false)
+    const clickHandler = () => {setNavMenu(!navMenu)}
+
     return (
         <header className='main-header'>
             <nav className='nav nav-container'>
                 <div className='logo'>
                     <img src={logo} alt='logo' />
                 </div>
-                <div className='menu'>menu</div>
-                <ul className='nav__list'>
+                <div className={navMenu ? 'nav__toggler nav__open' : 'nav__toggler'} onClick={clickHandler}>
+                    <div className="bar bar-one"></div>
+                    <div className="bar bar-two"></div>
+                    <div className="bar bar-three"></div>
+                </div>
+                <ul className={navMenu ? 'nav__list nav__menu' : 'nav__list'}>
                     <li className='nav__item'><a href="#"> پشتیبانی </a></li>
                     <li className='nav__item'><a href="#"> خدمات کسب و کارها </a></li>
                     <li className='nav__item'><a href="#"> فرصت های شغلی  </a></li>
